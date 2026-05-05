62d73dc213398c57f53fdf1804a87b163df3d790
sineyed <sineyed@users.noreply.github.com>
2026-05-05 18:15:06 +0000

    Update to v0.8.1

--- Formula/aethertune.rb
+++ Formula/aethertune.rb
@@ -1,10 +1,26 @@
 class Aethertune < Formula
   desc "Terminal-based internet radio player with real-time audio visualization, built in Rust"
   homepage "https://github.com/nevermore23274/AetherTune"
-  url "https://github.com/nevermore23274/AetherTune/releases/download/v0.8.0/AetherTune-v0.8.0-linux-x86_64.tar.gz"
-  sha256 "dd4d2e4389d07557af4cc65b73b101af522080c230f9a1ba6ee1c8743fccd317"
   license "MIT"
-  version "0.8.0"
+  version "0.8.1"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/nevermore23274/AetherTune/releases/download/v0.8.1/AetherTune-v0.8.1-macos-aarch64.tar.gz"
+      sha256 "9e323bb74dab89adb0708a1bfaf9f89c4ed6d1e8e48702b36faa06f97b3296ad"
+    end
+    on_intel do
+      url "https://github.com/nevermore23274/AetherTune/releases/download/v0.8.1/AetherTune-v0.8.1-macos-x86_64.tar.gz"
+      sha256 "dccb4c5a253e90d3657cf5e45849404953ac9389e2040b7252843d4f9e198399"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/nevermore23274/AetherTune/releases/download/v0.8.1/AetherTune-v0.8.1-linux-x86_64.tar.gz"
+      sha256 "f286fe9827cf4c4a4c39d4519a022216602da4dce2a4611fb233ba5c8f2b63a8"
+    end
+  end
 
   depends_on "mpv"
 
