d92c30dda1e3aaf87b82a9acaa35aa9153fe7e62
sineyed <sineyed@users.noreply.github.com>
2026-09-01 03:05:07 +0000

    Update to v0.11.1

--- Formula/aethertune.rb
+++ Formula/aethertune.rb
@@ -7,18 +7,18 @@ class Aethertune < Formula
   on_macos do
     on_arm do
       url "https://github.com/nevermore23274/AetherTune/releases/download/v0.11.1/AetherTune-v0.11.1-macos-aarch64.tar.gz"
-      sha256 "9ab00ac7b3b898b95d56e8c8aba2ee23b31914b22b98551a12d92cafb8a5d7a7"
+      sha256 "af1e3b073c73ea2758f12e22fbe14d1370d95805af1fc95ff631e5f815bb19ff"
     end
     on_intel do
       url "https://github.com/nevermore23274/AetherTune/releases/download/v0.11.1/AetherTune-v0.11.1-macos-x86_64.tar.gz"
-      sha256 "58a38ab979c0490f5608fbb186e02612923aa314962b858a83374943a9d43dae"
+      sha256 "45cdcca984d67ec854c89467fcac3ad4aaacfb69393c780d729415814f61a3e5"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/nevermore23274/AetherTune/releases/download/v0.11.1/AetherTune-v0.11.1-linux-x86_64.tar.gz"
-      sha256 "521e6b9b5f78212ac888ff44f380df5fca7b3c2880320ff8e685d32a2d344b03"
+      sha256 "3b226ee86d06db3dafc0a14b495d7b1cfa325c387203b31dffeade5990c97463"
     end
   end
 
